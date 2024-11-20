64338bb9ac398cd7aa5726ee88443c2f0b560c7b
ArthurThz <theodorodev@outlook.com>
2024-11-20 02:56:38 -0300

    feat: add initial params to createNewAppointment method

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import { fastify } from "fastify";
 import cors from "@fastify/cors";
 import { Animals } from "./src/Animals/animals.js";
 import { User } from "./src/Authentication/user.js";
+import { Appointments } from "./src/Appointments/appointments.js";
 const server = fastify();
 
 const animals = new Animals();
 const user = new User();
+const appointment = new Appointments()
 server.register(cors, {
   origin: "*",
 });
@@ -15,7 +17,6 @@ server.listen({
 
 server.get("/Horses", async (request, reply) => {
   const response = await animals.getHorses();
-  console.log(response);
   reply.code(200).send(response);
 });
 server.get("/SupportAnimals", async (request, reply) => {
@@ -46,7 +47,6 @@ server.post("/Register", async (request, reply) => {
   const response = await user.register(userData);
   const { code, message } = response;
 
-  console.log(code, message);
   reply.code(code).send(message);
 });
 
@@ -55,6 +55,16 @@ server.post("/Login", async (request, reply) => {
 
   const response = await user.login(email, senha);
 
+  const { code, message } = response;
+
+  reply.code(code).send(message);
+});
+
+server.post("/NewAppointment", async (request, reply) => {
+  const { userId, animalId} = request.body;
+
+  const response = await appointment.createAppointment(userId,animalId)
+  console.log(response)
   const { code, message } = response;
 
   reply.code(code).send(message);
--- src/Appointments/appointments.js
+++ src/Appointments/appointments.js
@@ -1,7 +1,28 @@
 import { randomUUID } from "node:crypto";
+import { sql } from "../../db.js";
 
 export class Appointments {
-  async createAppointment() {
+  async createAppointment(userId,animalId) {
     const uuid = randomUUID();
+
+    const getUserInfo = await sql`select nome,cpf, deficiencia, nivel_suporte from usuarios where uuid = ${userId}`
+
+    const {nome, cpf,deficiencia,nivel_suporte } = getUserInfo[0]
+
+    const getAnimalInfo = await sql` select nome from animais where codanimal = ${animalId}`
+    const nomeAnimal = getAnimalInfo[0].nome
+
+    const dataConsulta = new Date()
+    const createNewAppointmentResponse = await sql`insert into consultas
+     (id_consulta,nome_usuario,cpf_usuario,deficiencia,nivel_suporte,id_animal,data_consulta,horario,nome_animal)
+    values
+    (${uuid},${nome},${cpf},${deficiencia},${nivel_suporte},${animalId},${dataConsulta},'14:00',${nomeAnimal}) `
+  
+    // console.log(createNewAppointmentResponse)
+     return {
+      code:200,
+      message:"TESTE",
+      content:createNewAppointmentResponse
+     }
   }
 }
